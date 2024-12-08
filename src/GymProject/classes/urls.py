88bf38e33ae4dc2a902f3af0d7e51b018d3a7147
Prajwal148 <garlaprajwal1408@gmail.com>
2024-12-07 21:35:36 -0500

    Delete src/GymProject directory

--- src/GymProject/classes/urls.py
+++
@@ -1,7 +0,0 @@
-from . import views
-from django.urls import path
-
-urlpatterns = [
-    path('class/',views.classes,name='classes'),
-    path('bookings/',views.bookings,name='bookings'),
-]
--- src/GymProject/user/urls.py
+++
@@ -1,7 +0,0 @@
-from . import views
-from django.urls import path
-
-urlpatterns = [
-    path('profile/',views.profile_view,name='profile'),
-    path('userprofile/',views.userprofile,name='userprofile')
-]
