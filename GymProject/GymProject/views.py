1d16914d7f681b36830f1e8d42990b23289e4099
Prajwal148 <garlaprajwal1408@gmail.com>
2024-12-07 22:04:32 -0500

    Update views.py

--- GymProject/GymProject/views.py
+++ GymProject/GymProject/views.py
@@ -4,7 +4,7 @@ from django.template import loader
 
 # Create your views here.
 def index(request):
-    template = loader.get_template('index.html')
+    template = loader.get_template('Index.html')
     context={}
     return HttpResponse(template.render(context,request))
 
